a72441847313395c7db953ec43f13b31ba98f749
Cheaurizar <cheaurizard@proton.me>
2026-05-24 15:23:33 +0200

    modificiation de la barre de shell

--- modules/home/starship.nix
+++ modules/home/starship.nix
@@ -1,83 +1,139 @@
 { pkgs, ... }:
+let
+  palette = {
+    color_fgw = "#FFFFFF";
+    color_fgb = "#000000";
+    color_bg1 = "#3c3836";
+    color_blue = "#0A84FF";
+    color_green = "#00FF6C";
+    color_red = "#CC0000";
+    color_pink = "#FF0055";
+    color_white = "#FFFFFF";
+    color_cyan = "#0000AA";
+  };
 
+  defaultFg = "color_fgw";
+
+  # Segments de gauche : affichés dans l'ordre, séparateurs pointant vers la droite
+  leftSegments = [
+    { bg = "color_red";   fg = "color_fgw"; modules = [ "directory" ]; }
+    { bg = "color_green"; fg = "color_fgb"; modules = [ "cmd_duration" ]; }
+    { bg = "color_blue";  fg = "color_fgw"; modules = [ "git_branch" "git_status" ]; }
+  ];
+
+  # Segments de droite : affichés dans l'ordre, séparateurs pointant vers la gauche
+  rightSegments = [
+    { bg = "color_pink"; fg = "color_fgw"; modules = [ "python" "container" ]; }
+    { bg = "color_bg1";  fg = "color_fgw"; modules = [ "time" ]; }
+  ];
+
+  # Tous les segments réunis, pour que styleOfModule fonctionne quel que soit le côté
+  segments = leftSegments ++ rightSegments;
+
+  # Séparateurs powerline
+  sepLeftFilled  = "";   # triangle plein pointant à droite (ouvre un segment à gauche)
+  sepLeftEnd     = "";   # triangle plein pointant à droite (ferme un segment à gauche)
+  sepRightStart  = "";   # triangle plein pointant à gauche (ouvre un segment à droite)
+  sepRightEnd    = "";   # triangle plein pointant à gauche (ferme un segment à droite)
+
+  neutral = "color_white";
+
+  segFg = seg: if seg ? fg then seg.fg else defaultFg;
+  styleOf = seg: "bg:${seg.bg} fg:${segFg seg}";
+
+  segOf = m: builtins.head (builtins.filter (s: builtins.elem m s.modules) segments);
+  styleOfModule = m: styleOf (segOf m);
+
+  modulesStr = seg: builtins.concatStringsSep "" (map (m: "$" + m) seg.modules);
+
+  # Segment gauche : triangle droite, contenu, triangle droite
+  buildLeftSegment = seg:
+    ''[${sepLeftFilled}](fg:${neutral} bg:${seg.bg})${modulesStr seg}[${sepLeftEnd}](fg:${seg.bg} bg:${neutral})'';
+
+  # Segment droit : triangle gauche, contenu, triangle gauche
+  buildRightSegment = seg:
+    ''[${sepRightStart}](fg:${seg.bg} bg:${neutral})${modulesStr seg}[${sepRightEnd}](fg:${neutral} bg:${seg.bg})'';
+
+  leftBody  = builtins.concatStringsSep "" (map buildLeftSegment  leftSegments);
+  rightBody = builtins.concatStringsSep "" (map buildRightSegment rightSegments);
+
+  # $fill étire un caractère sur toute la largeur dispo entre gauche et droite
+  promptFormat =
+    "[${sepLeftFilled}](fg:color_bg1 bg:${neutral})"
+    + leftBody
+    + "[${sepLeftFilled}](bg:none fg:${neutral})"
+    + "$fill"
+    + "[${sepRightEnd}](bg:none fg:${neutral})"
+    + rightBody
+    + "[${sepRightEnd}](fg:color_bg1 bg:${neutral})"
+    + "$line_break"
+    + "[${sepLeftFilled}](fg:${neutral})";
+in
 {
   programs.starship = {
     enable = true;
-
     settings = {
       "$schema" = "https://starship.rs/config-schema.json";
+      format = promptFormat;
+      palette = "gruvbox_dark";
+      palettes.gruvbox_dark = palette;
 
-      format = ''[](bg:color_white fg:color_bg1)[](bg:color_red fg:color_white)$directory[](bg:color_white fg:color_red)[](bg:color_green fg:color_white)$cmd_duration[](fg:color_green bg:color_white)[](bg:color_blue fg:color_white)$git_branch$git_status[](fg:color_blue bg:color_white)[](fg:color_white bg:color_pink)$python$rust$container[](fg:color_pink bg:color_white)[](fg:color_white bg:color_bg1)$time[](fg:color_bg1 bg:color_white)[](fg:color_white)$line_break[](fg:color_bg1 bg:color_white)[](fg:color_white) '';
+      # Le module fill dessine la barre de liaison entre les deux côtés
+      fill = {
+        symbol = "─";
+        style = "fg:${neutral}";
+      };
 
-    palette = "gruvbox_dark";
+      username = {
+        show_always = true;
+        style_user = styleOfModule "directory";
+        style_root = styleOfModule "directory";
+        format = "[ $user ]($style)";
+      };
 
-    palettes.gruvbox_dark = {
-      color_fg0 = "#fbf1c7";
-      color_bg1 = "#3c3836";
-      color_blue = "#0000AA";
-      color_green = "#008800";
-      color_red = "#CC0000";
-      color_pink = "#FF0055";
-      color_white = "#FFFFFF";
-      color_cyan = "0000AA";
+      directory = {
+        style = styleOfModule "directory";
+        format = "[ $path ]($style)";
+        truncation_length = 7;
+        truncation_symbol = "…/";
+      };
+
+      git_branch = {
+        style = styleOfModule "git_branch";
+        always_show_remote = false;
+        symbol = " ";
+        format = "[ $symbol $branch(:$remote_branch) ]($style)";
+      };
+
+      git_status = {
+        style = styleOfModule "git_status";
+        format = "[($all_status$ahead_behind) ]($style)";
+      };
+
+      container = {
+        style = styleOfModule "container";
+        format = "[$symbol \\[$name\\]]($style)";
+      };
+
+      python = {
+        style = styleOfModule "python";
+        format = "[(\\($virtualenv\\)) ]($style)";
+      };
+
+      time = {
+        disabled = false;
+        time_format = "%R";
+        style = styleOfModule "time";
+        format = "[ $time ]($style)";
+      };
+
+      cmd_duration = {
+        disabled = false;
+        min_time = 0;
+        show_milliseconds = true;
+        style = styleOfModule "cmd_duration";
+        format = "[ $duration ]($style)";
+      };
     };
-
-    username = {
-      show_always = true;
-      style_user = "bg:color_red fg:color_fg0";
-      style_root = "bg:color_red fg:color_fg0";
-      format = "[ $user ]($style)";
-    };
-
-    directory = {
-      style = "fg:color_fg0 bg:color_red";
-      format = "[ $path ]($style)";
-      truncation_length = 7;
-      truncation_symbol = "…/";
-    };
-
-    git_branch = {
-      style = "bg:color_blue";
-      always_show_remote = true;
-      format = "[[ $branch | ($remote_branch) | ](fg:color_fg0 bg:color_blue)]($style)";
-    };
-
-    git_status = {
-      style = "bg:color_blue";
-      format = "[[($all_status$ahead_behind )](fg:color_fg0 bg:color_blue)]($style)";
-    };
-
-    container = {
-      style = "bg:color_pink";
-      format = "[$symbol \[$name\]]($style)";
-    };
-
-    time = {
-      disabled = false;
-      time_format = "%R";
-      style = "bg:color_bg1";
-      format = "[[ $time ](fg:color_fg0 bg:color_bg1)]($style)";
-    };
-
-    python = {
-      style ="bg:color_pink";
-      format = "[[(\($virtualenv\)) ]( bg:color_pink)]($style)";
-    };
-
-    rust = {
-      style ="bg:color_pink";
-      format = "[$version]($style)";  
-    };
-
-    cmd_duration = {
-      disabled = false;
-      min_time = 0;
-      show_milliseconds = true;
-      style = "bg:color_green";
-      format = "[ $duration ]($style)";
-    };
-
-
   };
-};
 }
