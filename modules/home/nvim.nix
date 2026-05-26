6db2cc7d28aa2dbf39ec0f42e0ac314c9233d7a7
Galicain <cheaurizard@proton.me>
2026-05-26 23:12:35 +0200

    feat(nvim): manage neovim configuration through nixvim
    Adds the nix-community/nixvim flake input pinned to nixpkgs, and
    rewrites modules/home/nvim.nix to use the nixvim home-manager module
    instead of the bare programs.neovim option.
    
    The new configuration declares editor options, the catppuccin
    colorscheme, leader-based keymaps, and a plugin set covering
    telescope, treesitter, lualine, gitsigns, which-key, autopairs and
    comment. LSP is enabled with nixd for Nix, and completion is wired
    through cmp with nvim_lsp, path and buffer sources.
    
    This moves neovim from an ad-hoc package install to a fully
    declarative setup that rebuilds reproducibly with the rest of the
    system.

--- modules/home/nvim.nix
+++ modules/home/nvim.nix
@@ -1,7 +1,102 @@
-{ ... }:
+# modules/home/nvim.nix
+{ inputs, ... }:
 {
-  programs.neovim = {
+  imports = [ inputs.nixvim.homeModules.nixvim ];
+
+  programs.nixvim = {
     enable = true;
+    defaultEditor = true;
+
+    viAlias = true;
     vimAlias = true;
+
+    colorschemes.catppuccin.enable = true;  # ou tokyonight, gruvbox, etc.
+
+    opts = {
+      number = true;
+      relativenumber = true;
+      shiftwidth = 2;
+      tabstop = 2;
+      expandtab = true;
+      smartindent = true;
+      wrap = false;
+      ignorecase = true;
+      smartcase = true;
+      termguicolors = true;
+      scrolloff = 8;
+      signcolumn = "yes";
+      updatetime = 50;
+    };
+
+    globals.mapleader = " ";
+
+    plugins = {
+      lualine.enable = true;
+      telescope = {
+        enable = true;
+        keymaps = {
+          "<leader>ff" = "find_files";
+          "<leader>fg" = "live_grep";
+          "<leader>fb" = "buffers";
+        };
+      };
+      treesitter = {
+        enable = true;
+        settings = {
+          highlight.enable = true;
+          indent.enable = true;
+        };
+      };
+      web-devicons.enable = true;
+      gitsigns.enable = true;
+      which-key.enable = true;
+      nvim-autopairs.enable = true;
+      comment.enable = true;
+
+      # LSP
+      lsp = {
+        enable = true;
+        servers = {
+          nixd.enable = true;          # LSP Nix
+          # lua_ls.enable = true;
+          # rust_analyzer = { enable = true; installCargo = true; installRustc = true; };
+          # ts_ls.enable = true;
+          # pyright.enable = true;
+        };
+        keymaps = {
+          lspBuf = {
+            "gd" = "definition";
+            "gr" = "references";
+            "K" = "hover";
+            "<leader>rn" = "rename";
+            "<leader>ca" = "code_action";
+          };
+        };
+      };
+
+      # Complétion
+      cmp = {
+        enable = true;
+        autoEnableSources = true;
+        settings = {
+          sources = [
+            { name = "nvim_lsp"; }
+            { name = "path"; }
+            { name = "buffer"; }
+          ];
+          mapping = {
+            "<CR>" = "cmp.mapping.confirm({ select = true })";
+            "<Tab>" = "cmp.mapping.select_next_item()";
+            "<S-Tab>" = "cmp.mapping.select_prev_item()";
+          };
+        };
+      };
+    };
+
+    keymaps = [
+      { mode = "n"; key = "<leader>w"; action = ":w<CR>"; }
+      { mode = "n"; key = "<leader>q"; action = ":q<CR>"; }
+      { mode = "n"; key = "<Esc>"; action = ":noh<CR>"; }
+    ];
   };
 }
