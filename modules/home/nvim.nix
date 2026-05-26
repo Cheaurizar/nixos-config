648ce5aea776d4e7e3c06c506c2dd14f69ed7826
Galicain <cheaurizard@proton.me>
2026-05-26 23:27:45 +0200

    feat(nvim): expand nixvim config with file tree, more LSPs and keymaps
    Builds out the previously minimal Neovim setup into something usable
    day to day. Adds nvim-tree as a file explorer, indent-blankline for
    visual indent guides, and extends the LSP server list with nil_ls,
    lua_ls, pyright and rust_analyzer (replacing the lone nixd).
    Treesitter now ships an explicit grammar set covering the languages
    actually edited in this repo.
    
    Keymaps are reorganised around <Space> as leader with descriptions
    attached (for which-key), and dedicated bindings are added for the
    file explorer, Telescope find/grep, and the most common LSP actions.
    A small TextYankPost autocmd is added to highlight yanked regions.
    
    The file is also reformatted to the standard nixpkgs style (explicit
    config/pkgs/inputs args, attribute ordering) to make further edits
    less noisy.

--- modules/home/nvim.nix
+++ modules/home/nvim.nix
@@ -1,102 +1,196 @@
-# modules/home/nvim.nix
-{ inputs, ... }:
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
+
 {
   imports = [ inputs.nixvim.homeModules.nixvim ];
 
+  # Configuration Nixvim basique pour Neovim.
   programs.nixvim = {
     enable = true;
-    defaultEditor = true;
 
+    # Alias pratiques
     viAlias = true;
     vimAlias = true;
+    defaultEditor = true;
 
-    colorschemes.catppuccin.enable = true;  # ou tokyonight, gruvbox, etc.
+    # Leader key
+    globals.mapleader = " ";
+    globals.maplocalleader = " ";
 
+    # Options Neovim classiques
     opts = {
       number = true;
       relativenumber = true;
-      shiftwidth = 2;
+      mouse = "a";
+      clipboard = "unnamedplus";
+
       tabstop = 2;
+      shiftwidth = 2;
+      softtabstop = 2;
       expandtab = true;
       smartindent = true;
+
       wrap = false;
       ignorecase = true;
       smartcase = true;
       termguicolors = true;
-      scrolloff = 8;
       signcolumn = "yes";
-      updatetime = 50;
+      scrolloff = 8;
+      updatetime = 250;
     };
 
-    globals.mapleader = " ";
+    # Thème + barre de statut
+    colorschemes.catppuccin = {
+      enable = true;
+      settings.flavour = "mocha";
+    };
 
     plugins = {
       lualine.enable = true;
-      telescope = {
+      web-devicons.enable = true;
+
+      # Explorateur de fichiers
+      nvim-tree = {
         enable = true;
-        keymaps = {
-          "<leader>ff" = "find_files";
-          "<leader>fg" = "live_grep";
-          "<leader>fb" = "buffers";
-        };
+        view.width = 35;
       };
+
+      # Recherche de fichiers / grep
+      telescope.enable = true;
+
+      # Syntax highlighting avancé
       treesitter = {
         enable = true;
         settings = {
           highlight.enable = true;
           indent.enable = true;
         };
+        grammarPackages = with config.programs.nixvim.plugins.treesitter.package.builtGrammars; [
+          nix
+          lua
+          python
+          rust
+          bash
+          json
+          yaml
+          toml
+          markdown
+        ];
       };
-      web-devicons.enable = true;
-      gitsigns.enable = true;
-      which-key.enable = true;
-      nvim-autopairs.enable = true;
-      comment.enable = true;
 
-      # LSP
-      lsp = {
+      indent-blankline = {
         enable = true;
-        servers = {
-          nixd.enable = true;          # LSP Nix
-          # lua_ls.enable = true;
-          # rust_analyzer = { enable = true; installCargo = true; installRustc = true; };
-          # ts_ls.enable = true;
-          # pyright.enable = true;
-        };
-        keymaps = {
-          lspBuf = {
-            "gd" = "definition";
-            "gr" = "references";
-            "K" = "hover";
-            "<leader>rn" = "rename";
-            "<leader>ca" = "code_action";
+        settings = {
+          indent = {
+            char = "│";
+          };
+          scope = {
+            enabled = true;
+            show_start = true;
+            show_end = true;
           };
         };
       };
 
-      # Complétion
+      # Language server protocol
+      lsp = {
+        enable = true;
+        servers = {
+          nil_ls.enable = true;
+          lua_ls.enable = true;
+          pyright.enable = true;
+          rust_analyzer = {
+            enable = true;
+            installCargo = true;
+            installRustc = true;
+          };
+        };
+      };
+
+      # Autocomplétion basique
       cmp = {
         enable = true;
-        autoEnableSources = true;
         settings = {
           sources = [
             { name = "nvim_lsp"; }
             { name = "path"; }
             { name = "buffer"; }
           ];
-          mapping = {
-            "<CR>" = "cmp.mapping.confirm({ select = true })";
-            "<Tab>" = "cmp.mapping.select_next_item()";
-            "<S-Tab>" = "cmp.mapping.select_prev_item()";
-          };
         };
       };
+
+      # Git / aide aux raccourcis / parenthèses auto
+      gitsigns.enable = true;
+      which-key.enable = true;
+      nvim-autopairs.enable = true;
     };
 
+    # Raccourcis clavier simples
     keymaps = [
-      { mode = "n"; key = "<leader>w"; action = ":w<CR>"; }
-      { mode = "n"; key = "<leader>q"; action = ":q<CR>"; }
-      { mode = "n"; key = "<Esc>"; action = ":noh<CR>"; }
+      {
+        mode = "n";
+        key = "<leader>w";
+        action = "<cmd>w<CR>";
+        options.desc = "Sauvegarder";
+      }
+      {
+        mode = "n";
+        key = "<leader>q";
+        action = "<cmd>q<CR>";
+        options.desc = "Quitter";
+      }
+      {
+        mode = "n";
+        key = "<leader>e";
+        action = "<cmd>NvimTreeToggle<CR>";
+        options.desc = "Ouvrir/fermer l'explorateur";
+      }
+      {
+        mode = "n";
+        key = "<leader>ff";
+        action = "<cmd>Telescope find_files<CR>";
+        options.desc = "Chercher un fichier";
+      }
+      {
+        mode = "n";
+        key = "<leader>fg";
+        action = "<cmd>Telescope live_grep<CR>";
+        options.desc = "Chercher dans le projet";
+      }
+      {
+        mode = "n";
+        key = "<leader>ca";
+        action = "vim.lsp.buf.code_action";
+        lua = true;
+        options.desc = "Code action LSP";
+      }
+      {
+        mode = "n";
+        key = "gd";
+        action = "vim.lsp.buf.definition";
+        lua = true;
+        options.desc = "Aller à la définition";
+      }
+      {
+        mode = "n";
+        key = "K";
+        action = "vim.lsp.buf.hover";
+        lua = true;
+        options.desc = "Documentation LSP";
+      }
     ];
+
+    # Petit confort : highlight après yank
+    extraConfigLua = ''
+      vim.api.nvim_create_autocmd("TextYankPost", {
+        callback = function()
+          vim.highlight.on_yank({ timeout = 150 })
+        end,
+      })
+    '';
   };
 }
