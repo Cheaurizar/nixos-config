4c6f496d0ee9a4d80ce82fccedc04c6474f68b7b
Galicain <cheaurizard@proton.me>
2026-07-10 22:39:52 +0200

    add codeberg on the ssh config

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -2,17 +2,13 @@
 {
   programs.ssh = {
     enable = true;
-
     enableDefaultConfig = false;
-
     matchBlocks = {
       "*" = {
         addKeysToAgent = "1h";
-
         controlMaster = "auto";
         controlPath = "~/.ssh/control-%r@%h:%p";
         controlPersist = "10m";
-
         forwardAgent = false;
         compression = false;
         serverAliveInterval = 0;
@@ -20,7 +16,6 @@
         hashKnownHosts = false;
         userKnownHostsFile = "~/.ssh/known_hosts";
       };
-
       github = {
         host = "github.com";
         hostname = "ssh.github.com";
@@ -29,8 +24,14 @@
         identityFile = "~/.ssh/id_ed25519";
         identitiesOnly = true;
       };
+      codeberg = {
+        host = "codeberg.org";
+        hostname = "codeberg.org";
+        user = "git";
+        identityFile = "~/.ssh/id_ed25519_codeberg";
+        identitiesOnly = true;
+      };
     };
   };
-
   services.ssh-agent.enable = true;
 }
